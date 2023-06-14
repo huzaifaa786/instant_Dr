ac2dcfaa479fa7d1e5b76bf894877015e0b9ad2a
Huzaifa Aslam <tritechuzaifa@gmail.com>
2023-06-14 15:25:28 +0500

    ali

--- lib/screen/doctor_list/doctor_list.dart
+++ lib/screen/doctor_list/doctor_list.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:instant_doctor/static/iconinputfield.dart';
 
 class DoctorList extends StatefulWidget {
   const DoctorList({super.key});
@@ -14,8 +16,19 @@ class _DoctorListState extends State<DoctorList> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Column(
-        children: [],
+          child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [SvgPicture.asset('assets/images/backArrow.svg')],
+              ),
+            ),
+            IconInputField(imageIcon: Icons.search,)
+          ],
+        ),
       )),
     );
   }
