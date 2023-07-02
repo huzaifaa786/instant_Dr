6fc68ed7f4b294885bfbed536f27a8cbd5a73e15
itriteckodes <tritechuzaifa@gmail.com>
2023-07-02 15:46:51 +0500

    changes

--- android/app/src/main/kotlin/com/example/instant_doctor/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/instant_doctor/MainActivity.kt
@@ -4,8 +4,4 @@ import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.android.FlutterFragmentActivity
 
 class MainActivity: FlutterFragmentActivity() {
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 9ef453d94cc9246f46296c9216a1259683dafc13
